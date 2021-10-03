c12765975196d4ce74561b340073027e4fa12a93
Linneu Lopes <linneu@golaw.com.br>
2021-10-03 18:08:13 -0300

    change header data

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,9 @@
 import resume from '../assets/pdf/resume.pdf'
 
 export const headerData = {
-    name: 'Jane Doe',
+    name: 'Linneu Lopes',
     title: "Web Developer",
     desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
-    image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
+    image: 'https://avatars.githubusercontent.com/u/16892488?s=400&u=c134ec60abfaa7f604b0e160b99d349fad02a755&v=4',
     resumePdf: resume
 }
