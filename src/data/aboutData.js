5e08349c3279fb7405a4e1321242e943b03aaccd
Linneu Lopes <linneu.dm@gmail.com>
2022-08-02 03:35:53 -0300

    fix english language

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
-    title: "Who I am",
-    description1: "My name is Linneu. I'm a web developer and I'm live Brazil.",
+    title: "Who am I",
+    description1: "Hello, I'm Linneu! I am a full stack developer currently living in Brazil.",
     description2: " I’m currently working with Vuejs. I'm looking to colaborate on Open Source. If you need to talk with me, send me a email ❤.",
     image: 1
 }
--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Linneu Lopes',
     title: "Web Developer",
     desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
-    image: 'https://avatars.githubusercontent.com/u/16892488?s=400&u=c134ec60abfaa7f604b0e160b99d349fad02a755&v=4',
+    image: 'https://pbs.twimg.com/profile_images/1533858280161820672/31GBHwap_400x400.jpg',
     resumePdf: resume
 }
